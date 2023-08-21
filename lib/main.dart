97bd53ab31ca1f71656c8df7cae63569a2a8edca
Mark Sibbald <marksibbald@Marks-MacBook-Pro.local>
2023-08-21 07:57:13 +0100

    Correct icons showing

--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,47 @@ class StepperExample extends StatefulWidget {
 class _StepperExampleState extends State<StepperExample> {
   int _index = 0;
 
+  final List<Step> steps = [
+    Step(
+      label: Text("Checking sequencing data"),
+      title: SizedBox.shrink(),
+      content: Text("Content for Step 1"),
+      state: StepState.editing,
+    ),
+    Step(
+      label: Text("Sequencing data OK"),
+      title: SizedBox.shrink(),
+      content: Text("Test"),
+      state: StepState.complete,
+    ),
+    Step(
+      label: Text("Analysing sequencing data"),
+      title: SizedBox.shrink(),
+      content: Text("Content for Step 3"),
+      state: StepState.editing,
+    ),
+    Step(
+      label: Text("Download recommendations treatment report"),
+      title: SizedBox.shrink(),
+      content: Text("Content for Step 4"),
+      state: StepState.complete,
+    ),
+  ];
+
   Widget customIconBuilder(int index, StepState stepState) {
-    print(index);
-    switch (index) {
-      case 0:
-        return const Icon(Icons.lock);
+    final label = steps[index].label as Text;
+
+    switch (label.data) {
+      case "Checking sequencing data":
+        return const Icon(Icons.science_outlined);
+      case "Download recommendations treatment report":
+        return const Icon(Icons.task_outlined);
+      case "Analysing sequencing data":
+        return const Icon(Icons.query_stats_outlined);
+      case "Sequencing data OK":
+        return const Icon(Icons.thumb_up_alt_outlined);
       default:
-        return const Icon(Icons.error);
+        return const Icon(Icons.question_mark_outlined);
     }
   }
 
@@ -65,26 +99,7 @@ class _StepperExampleState extends State<StepperExample> {
           _index = index;
         });
       },
-      steps: <Step>[
-        Step(
-          label: Text("Step 1 title"),
-          title: SizedBox.shrink(),
-          content: Container(
-            alignment: Alignment.centerLeft,
-            child: Text("Content for Step 1"),
-          ),
-          state: StepState.editing,
-        ),
-        Step(
-          label: Text("Step 2 title"),
-          title: SizedBox.shrink(),
-          content: Container(
-            alignment: Alignment.centerLeft,
-            child: Text("Content for Step 2"),
-          ),
-          state: StepState.complete,
-        ),
-      ],
+      steps: steps,
     );
   }
 }
